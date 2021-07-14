f91de6327b4974f9c6ae41fa9453479bbcf89521
tienbaopham <phamtienbao11cntt22@gmail.com>
2021-07-14 15:20:54 +0700

    Initial commit

---
+++ MetromecanicaApp/MainWindowView.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Caliburn.Micro;
+
+namespace MetromecanicaApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindowView : Window
+    {
+        public MainWindowView()
+        {
+            InitializeComponent();
+
+            DirectoryInfo forder = new DirectoryInfo("Assets");
+            string linkImage = string.Empty;
+            if (forder.Exists)
+            {
+                foreach (FileInfo item in forder.GetFiles())
+                {
+                    if (".jpg".Contains(item.Extension.ToLower()))
+                    {
+                        linkImage = item.FullName;
+                    }
+                }
+            }
+
+            int maxlenght = 30;
+            var model = new MovieData[maxlenght];
+            for (int i = 0; i < maxlenght; i++)
+            {
+                model[i] = new MovieData();
+                model[i].Title = string.Format("Nombre{0}", i);
+                model[i].ImageData = LoadImage(linkImage);
+            }
+
+            this.TvBox.ItemsSource = model;
+        } 
+
+        // for this code image needs to be a project resource
+        private BitmapImage LoadImage(string filename)
+        {
+            return new BitmapImage(new Uri(filename));
+        }
+    }
+}
---
+++ MetromecanicaApp/MainWindowViewModel.cs
@@ -0,0 +1,76 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace MetromecanicaApp
+{
+    public class MainWindowViewModel: PropertyChangedBase
+    {
+        private Visibility isDisplayHome;
+
+        public Visibility IsDisplayHome
+        {
+            get { return isDisplayHome; }
+            set { isDisplayHome = value;
+                NotifyOfPropertyChange(() => IsDisplayHome);
+            }
+        }
+
+        private Visibility isDisplayNuevo;
+
+        public Visibility IsDisplayNuevo
+        {
+            get { return isDisplayNuevo; }
+            set
+            {
+                isDisplayNuevo = value;
+                NotifyOfPropertyChange(() => IsDisplayNuevo);
+            }
+        }
+
+        public MainWindowViewModel()
+        {
+            IsDisplayHome = Visibility.Visible;
+            IsDisplayNuevo = Visibility.Hidden;
+        }
+
+        public void Open_InicioViewModel()
+        {
+            IsDisplayHome = Visibility.Visible;
+            IsDisplayNuevo = Visibility.Hidden;
+        }
+
+        public void Open_NuevoViewModel()
+        {
+            IsDisplayHome = Visibility.Hidden;
+            IsDisplayNuevo = Visibility.Visible;
+        }
+
+        public void Open_AbrirViewModel()
+        {
+            IsDisplayHome = Visibility.Hidden;
+        }
+    }
+
+    public class MovieData
+    {
+        private string _Title;
+        public string Title
+        {
+            get { return this._Title; }
+            set { this._Title = value; }
+        }
+
+        private BitmapImage _ImageData;
+        public BitmapImage ImageData
+        {
+            get { return this._ImageData; }
+            set { this._ImageData = value; }
+        }
+    }
+}
